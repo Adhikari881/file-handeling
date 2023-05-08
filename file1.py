a5c5dfefe796ca495bb2ce2b71c5ff5d34e8a37f
Aman Sngh <amanadhikariaps@gmail.com>
2023-05-08 16:32:52 +0530

    isme/Users/pc2/opt/anaconda3/envs/my_env/bin/python "/Users/pc2/Desktop/file handel pro/file-handeling/file1.py"

--- file1.py
+++ file1.py
@@ -1,3 +1,24 @@
 """in this code i am trying to get a systematic way to get the data from clients to csv formatt"""
 
 "hera pheri"
+
+import csv
+import time
+import random
+
+samples = {"place":[{'data': random.randint(1,10), 'time': time.time()}]*5}
+
+
+with open('new.csv', 'w') as f:
+        fieldnames = ['data','time']
+        thewriter = csv.DictWriter(f, fieldnames = fieldnames)
+        
+        thewriter.writeheader()
+        for key, value in samples.items():
+            print(key)
+            for val in value:
+                packet = {'data':val['data'],
+                        'time':val['time']
+                        }
+                print(packet)
+                thewriter.writerow(packet)
